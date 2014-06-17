cfa3953b3dc8090a1d3d0820aa474d3d43779251
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-06-17 15:20:05 +0530

    add validation if textbox blank

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -8,13 +8,14 @@ class SmsController < ApplicationController
     nexmo = Nexmo::Client.new('751b4bd3', '520dd4e3')
     if params[:sms][:phone_number].present?
       response = nexmo.send_message! ({:to => "91#{params[:sms][:phone_number]}", :from => 'demo123', :text => 'Your authentication code is :- 12345'})
-
       if response
+        flash[:success] = "SMS Sent to #{params[:sms][:phone_number]}"
         redirect_to  new_sm_path
       else
         puts "===================not sent"
       end
     else
+      flash[:notice] = "Enter phone number"
       render :new
     end
   end
--- app/models/sms.rb
+++ app/models/sms.rb
@@ -1,3 +1,4 @@
 class Sms < ActiveRecord::Base
+  validates_presence_of :phone_number
   validates_uniqueness_of :phone_number
 end

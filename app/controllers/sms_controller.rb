774d612c96b1901891864693e78eba08fe957b33
parthivpatel <parthiv.patel@botreeconsulting.com>
2014-06-17 15:08:46 +0530

    get to post callback

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -6,15 +6,17 @@ class SmsController < ApplicationController
 
   def create
     nexmo = Nexmo::Client.new('751b4bd3', '520dd4e3')
-    response = nexmo.send_message! ({:to => "91#{params[:sms][:phone_number]}", :from => 'demo123', :text => 'Your authentication code is :- 12345'})
+    if params[:sms][:phone_number].present?
+      response = nexmo.send_message! ({:to => "91#{params[:sms][:phone_number]}", :from => 'demo123', :text => 'Your authentication code is :- 12345'})
 
-    if response
-      puts "******************sent"
+      if response
+        redirect_to  new_sm_path
+      else
+        puts "===================not sent"
+      end
     else
-      puts "===================not sent"
+      render :new
     end
-
-    redirect_to  new_sm_path
   end
 
   def callback
--- app/models/sms.rb
+++ app/models/sms.rb
@@ -1,4 +1,3 @@
 class Sms < ActiveRecord::Base
-  validates_presence_of :phone_number
   validates_uniqueness_of :phone_number
 end
